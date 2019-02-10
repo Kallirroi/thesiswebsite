d60bc9bb99b9bee0b9c9c20031160b413d1826c5
Kallirroi <kallirroi.retzepi@gmail.com>
2019-02-10 13:53:07 -0500

    Its easier if I have a twitter account and link to that. Recording, saving and playing recordings will be complicated.

--- src/LeftSide.js
+++ src/LeftSide.js
@@ -7,7 +7,8 @@ render() {
 	console.log(this.props.className)
 	return (
 	  	<div className={this.props.className}>
-        <p>Come speak to these objects in real life! You can find them below: </p>
+				<div className="offline">PARTICIPATE OFFLINE</div>
+        <p className="text">Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Donec quam felis, ultricies nec, pellentesque eu, pretium quis, sem. Nulla consequat massa quis enim. Donec pede justo, fringilla vel, aliquet nec, vulputate eget, arcu. In enim justo, rhoncus ut, imperdiet a, venenatis vitae, justo. Nullam dictum felis eu pede mollis pretium. Integer tincidunt. Cras dapibus. Vivamus elementum semper nisi. </p>
 				<div className="places">
 				<li> place # 1</li>
 				<li> place # 2</li>
--- src/RightSide.js
+++ src/RightSide.js
@@ -7,11 +7,14 @@ class RightSide extends React.Component {
 render() {
 	return (
 	  	<div className={this.props.className}>
-				<p>If you don't want to that, you can also leave your message below. </p>
-				<button className="record"> Record </button>
+				<div className="offline">PARTICIPATE ONLINE</div>
+				<p className="text">Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Donec quam felis, ultricies nec, pellentesque eu, pretium quis, sem. Nulla consequat massa quis enim. Donec pede justo, fringilla vel, aliquet nec, vulputate eget, arcu. In enim justo, rhoncus ut, imperdiet a, venenatis vitae, justo. Nullam dictum felis eu pede mollis pretium. Integer tincidunt. Cras dapibus. Vivamus elementum semper nisi. </p>
 
-				<div className="recordings">
-					<li> message #1 <span> download </span> </li>
+				<button className="record"> Tweet your response </button>
+
+				<div className="recordings"> {/* I have set this to display: none */}
+				Those who have participated online said:
+					<li> message #1 </li>
 					<li> message #2 </li>
 					<li> message #3 </li>
 					<li> message #4 </li>

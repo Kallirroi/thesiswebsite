a8c4932a46f55ea1a79a7827900110afd990179b
Kallirroi <kallirroi.retzepi@gmail.com>
2019-03-15 17:35:50 -0400

    more info on locations

--- src/RightSide.js
+++ src/RightSide.js
@@ -9,11 +9,10 @@ render() {
 			<p className="text">
 			You can find the nodes of the decentralized network in the locations listed below. Each object records and plays back audio messages. It also receives and sends data to and from the rest of the hyper-local network’s nodes. </p>
 			<div className="places">
-				<li> Media Lab (<a href="https://whereis.mit.edu/?go=E14">link</a>)</li>
-				<li> Student Center (<a href="https://whereis.mit.edu/?go=W20">link</a>)</li>
-				<li> Muddy Charles Pub (<a href="https://whereis.mit.edu/?go=50">link</a>)</li>
-				{/*<li> Dorm (<a href="">link</a>)</li>
-				<li> Dorm (<a href="">link</a>)</li>*/}
+				<li> Media Lab - third floor atrium (<a href="https://whereis.mit.edu/?go=E14">link</a>)</li>
+				<li> Student Center - second floor(<a href="https://whereis.mit.edu/?go=W20">link</a>)</li>
+				<li> Muddy Charles Pub - Memorial Dr. entrance (<a href="https://whereis.mit.edu/?go=50">link</a>)</li>
+				<li> MIT List Visual Arts Center - Ames Street entrance (<a href="https://whereis.mit.edu/?go=E15">link</a>)</li>
 			</div>
 			<span className="number"> 2 </span>
 			<p className="text">At each location, you can choose to either use the decentralized network, or one of the "Twitter objects". These objects are entry point to Twitter. They record audio messages, transcribe them and share them on Twitter. </p>
